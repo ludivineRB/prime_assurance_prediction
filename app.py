79cc3edb4b5857491c0fafa62ff08d6bfddfa24f
Ludivine.RB <ludivine.raby@gmail.com>
2025-01-15 01:22:12 +0100

    Fin de modélisation, début de app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+
+with open("model_pipeline.pkl", "rb") as file:
+    model = pickle.load(file)
+
+# X = df.drop(columns=["charges"]) 
+# y = df["charges"]
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=X['smoker'])
+#age,sex,bmi,children,smoker,region,charges
+input_data = pd.DataFrame({
+    "age": [20],
+    "sex": ["male"],
+    "bmi": [40],
+    "children": [2],
+    "smoker": ["yes"],
+    "region": ["northeast"]
+})
+# Test 
+prediction = model.predict(input_data)
+print(prediction)
+
+#C'est là ou il va nous manquer la transo en catégorie des bmi... le truc de victor à rajouter dans la pipeline
